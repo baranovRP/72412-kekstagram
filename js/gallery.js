de8d89f3a637c3a7d4a4f022e2532008e498e12c
Roman Baranov <baranov.r.p@gmail.com>
2016-02-13 17:19:27 +0100

    #module7-task1: remove handlers when gallery hidden;

--- js/gallery.js
+++ js/gallery.js
@@ -14,6 +14,10 @@
   function Gallery() {
     this.el = document.querySelector('.gallery-overlay');
     this._closeButton = document.querySelector('.gallery-overlay-close');
+
+    this._onEscPress = this._onEscPress.bind(this);
+    this._onOverlayClick = this._onOverlayClick.bind(this);
+    this._onCloseClick = this._onCloseClick.bind(this);
   }
 
   /**
@@ -22,33 +26,9 @@
   Gallery.prototype.show = function() {
     this.el.classList.remove('invisible');
 
-    /**
-     * Event handler, close gallery by clicking on close button.
-     */
-    this._closeButton.addEventListener('click', function(evt) {
-      evt.preventDefault();
-      this.hide();
-    }.bind(this));
-
-    /**
-     * Event handler, close gallery by clicking on overlay area.
-     */
-    this.el.addEventListener('click', function(evt) {
-      if (evt.target.classList.contains('gallery-overlay')) {
-        evt.preventDefault();
-        this.hide();
-      }
-    }.bind(this));
-
-    /**
-     * Event handler, close gallery by pressing on 'Esc' button.
-     */
-    document.addEventListener('keydown', function(evt) {
-      if (evt.keyCode === VK_ESCAPE) {
-        evt.preventDefault();
-        this.hide();
-      }
-    }.bind(this));
+    window.addEventListener('keydown', this._onEscPress);
+    this.el.addEventListener('click', this._onOverlayClick);
+    this._closeButton.addEventListener('click', this._onCloseClick);
   };
 
   /**
@@ -56,6 +36,41 @@
    */
   Gallery.prototype.hide = function() {
     this.el.classList.add('invisible');
+
+    window.removeEventListener('keydown', this._onEscPress);
+    this.el.removeEventListener('click', this._onOverlayClick);
+    this._closeButton.removeEventListener('click', this._onCloseClick);
+
+  };
+
+  /**
+   * Event handler, close gallery by pressing on 'Esc' button.
+   */
+  Gallery.prototype._onEscPress = function(evt) {
+    if (evt.keyCode === VK_ESCAPE) {
+      evt.preventDefault();
+      this.hide();
+    }
+  };
+
+  /**
+   * Event handler, close gallery by clicking on overlay area.
+   * @private
+   */
+  Gallery.prototype._onOverlayClick = function(evt) {
+    if (evt.target.classList.contains('gallery-overlay')) {
+      evt.preventDefault();
+      this.hide();
+    }
+  };
+
+  /**
+   * Event handler, close gallery by clicking on close button.
+   * @private
+   */
+  Gallery.prototype._onCloseClick = function(evt) {
+    evt.preventDefault();
+    this.hide();
   };
 
   window.Gallery = Gallery;
